5a5fdc386f7cb2d6e9994d08f1a47d76aff959a2
Kore5050 <127473459+Kore5050@users.noreply.github.com>
2023-03-15 13:40:19 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,9 @@
+class Addition{
+public static  void main(string []args){
+system.out.printin("The two number is:");
+int a=5;
+intb=6;
+system.out.printin(a+""+b);
+int sum=a+b;
+system.out,printin("The sum is ="+ sum);
+}}
---
+++ area.java
@@ -0,0 +1,8 @@
+class reactangle{
+public static void main(string args[]){
+int length=10;
+int breadth=20;
+ int area=length*breadth;
+system.out.printin("Area of rectangle is=" +area);
+}
+}
