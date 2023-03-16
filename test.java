c2ec41c3b712338f9cc8a29204866bacefdf8863
Kore5050 <127473459+Kore5050@users.noreply.github.com>
2023-03-16 09:49:56 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+// javaa ---OOPS , platform independent, Robust, Secure, High level
+
+class Test
+{
+	public static void main(String args[])
+
+{
+	System.out.println("Welcome to the world of Coding");
+	
+}
+}
