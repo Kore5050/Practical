813ed40f8cc71c81bca1227f3a086c046767e4b2
Kore5050 <127473459+Kore5050@users.noreply.github.com>
2023-03-16 09:47:57 +0530

    Add files via upload

---
+++ Data type.java
@@ -0,0 +1,29 @@
+lines (24 sloc)  608 Bytes
+
+class Datatype_Test
+{
+    int a;
+    short b;
+    long c;
+    double g;
+    float h;
+    char m;
+    void show()
+    {
+        System.out.println("int "+a);
+        System.out.println("short "+b);
+        System.out.println("long "+c);
+        System.out.println("double "+g);
+        System.out.println("float "+h);
+        System.out.println("char "+m);
+        
+    } 
+    
+    public static void main(String args[])
+    {
+        Datatype_Test object=new Datatype_Test();
+        System.out.println("The default values are goven below:");
+        object.show();
+        
+    }
+}
