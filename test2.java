171eea1f71858a3d19fcfb78a920ccb06d35e934
Kore5050 <127473459+Kore5050@users.noreply.github.com>
2023-03-16 09:50:36 +0530

    Add files via upload

---
+++ test2.java
@@ -0,0 +1,11 @@
+// javaa ---OOPS , platform independent, Robust, Secure, High level
+
+class Test
+{
+	public static void main(String args[])
+
+{
+	System.out.println("I am Kore");
+	System.out.println("Also knowm as Kore5050");
+}
+}
